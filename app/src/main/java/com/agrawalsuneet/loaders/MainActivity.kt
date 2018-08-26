cba303828ca5e9a203d18fcd26a8c71e6cde4f3a
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-08-26 15:57:32 +0100

    added loader

--- app/src/main/java/com/agrawalsuneet/loaders/MainActivity.kt
+++ app/src/main/java/com/agrawalsuneet/loaders/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_trailingcirculer)
+        setContentView(R.layout.activity_main_zee)
 
         supportActionBar!!.title = "TrailingCircularDotsLoader"
 
