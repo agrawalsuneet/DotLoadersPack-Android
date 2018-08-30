8cff504bed115fac61138d7bc0375ff3d5e059c9
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-08-31 00:00:43 +0100

    made circle view only stroke enabled

--- app/src/main/java/com/agrawalsuneet/loaders/MainActivity.kt
+++ app/src/main/java/com/agrawalsuneet/loaders/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_zee)
+        setContentView(R.layout.activity_main_alliance)
 
         supportActionBar!!.title = "ZeeLoader"
 
--- dotsloader/src/main/java/com/agrawalsuneet/dotsloader/basicviews/CircleView.kt
+++ dotsloader/src/main/java/com/agrawalsuneet/dotsloader/basicviews/CircleView.kt
@@ -13,13 +13,23 @@ import com.agrawalsuneet.dotsloader.R
 
 class CircleView : View {
 
-    var circleRadius: Int = 15
+    var circleRadius: Int = 30
+    var strokeWidth: Int = 0
 
     var circleColor: Int = 0
+    var drawOnlyStroke: Boolean = false
 
     constructor(context: Context, circleRadius: Int, circleColor: Int) : super(context) {
         this.circleRadius = circleRadius
         this.circleColor = circleColor
+    }
+
+    constructor(context: Context, circleRadius: Int, circleColor: Int, drawOnlyStroke: Boolean, strokeWidth: Int) : super(context) {
+        this.circleRadius = circleRadius
+        this.circleColor = circleColor
+
+        this.drawOnlyStroke = drawOnlyStroke
+        this.strokeWidth = strokeWidth
     }
 
     constructor(context: Context) : super(context)
@@ -38,13 +48,21 @@ class CircleView : View {
         this.circleRadius = typedArray.getDimensionPixelSize(R.styleable.CircleView_circleRadius, 30)
         this.circleColor = typedArray.getColor(R.styleable.CircleView_circleColor, 0)
 
+        this.drawOnlyStroke = typedArray.getBoolean(R.styleable.CircleView_circleDrawOnlystroke, false)
+
+        if (drawOnlyStroke) {
+            this.strokeWidth = typedArray.getDimensionPixelSize(R.styleable.CircleView_circleStrokeWidth, 0)
+        }
+
         typedArray.recycle()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-        setMeasuredDimension(2 * circleRadius, 2 * circleRadius)
+        val widthHeight = (2 * (circleRadius)) + strokeWidth
+
+        setMeasuredDimension(widthHeight, widthHeight)
     }
 
 
@@ -53,10 +71,18 @@ class CircleView : View {
 
         var paint = Paint()
         paint.isAntiAlias = true
-        paint.style = Paint.Style.FILL
+
+        if (drawOnlyStroke) {
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = strokeWidth.toFloat()
+        } else {
+            paint.style = Paint.Style.FILL
+        }
         paint.color = circleColor
 
-        canvas!!.drawCircle(circleRadius.toFloat(), circleRadius.toFloat(), circleRadius.toFloat(), paint)
+        val xyCordinates = (circleRadius + (strokeWidth / 2)).toFloat()
+
+        canvas!!.drawCircle(xyCordinates, xyCordinates, circleRadius.toFloat(), paint)
     }
 
 
