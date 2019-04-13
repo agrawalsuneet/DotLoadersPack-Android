3191e13ad8fda302881a72a7ca65af20a511db98
Suneet Agrawal <agrawalsuneet@gmail.com>
2019-04-14 00:05:06 +0530

    added layout

--- dotsloader/src/main/java/com/agrawalsuneet/dotsloader/loaders/BounceLoader.kt
+++ dotsloader/src/main/java/com/agrawalsuneet/dotsloader/loaders/BounceLoader.kt
@@ -8,6 +8,7 @@ import android.view.ViewTreeObserver
 import android.view.animation.*
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import com.agrawalsuneet.dotsloader.R
 import com.agrawalsuneet.dotsloader.basicviews.CircleView
 import com.agrawalsuneet.dotsloader.contracts.LoaderContract
 
@@ -21,20 +22,21 @@ class BounceLoader : LinearLayout, LoaderContract {
     var ballColor: Int = resources.getColor(android.R.color.holo_red_dark)
     var shadowColor: Int = resources.getColor(android.R.color.black)
 
-    var animDuration: Int = 2000
+    var showShadow: Boolean = true
+    var animDuration: Int = 1500
 
     private var relativeLayout: RelativeLayout? = null
 
     private var ballCircleView: CircleView? = null
     private var ballShadowView: CircleView? = null
 
-    private var calWidthHeight: Int = 0
+    private var calWidth: Int = 0
+    private var calHeight: Int = 0
 
     private val STATE_GOINGDOWN: Int = 0
     private val STATE_SQUEEZING: Int = 1
     private val STATE_RESIZING: Int = 2
     private val STATE_COMINGUP: Int = 3
-
 
     private var state: Int = STATE_GOINGDOWN
 
@@ -53,40 +55,55 @@ class BounceLoader : LinearLayout, LoaderContract {
     }
 
     override fun initAttributes(attrs: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.BounceLoader, 0, 0)
 
+        this.ballRadius = typedArray.getDimensionPixelSize(R.styleable.BounceLoader_bounce_ballRadius, 60)
+        this.ballColor = typedArray.getColor(R.styleable.BounceLoader_bounce_ballColor,
+                resources.getColor(android.R.color.holo_red_dark))
+
+        this.shadowColor = typedArray.getColor(R.styleable.BounceLoader_bounce_shadowColor,
+                resources.getColor(android.R.color.black))
+
+        this.showShadow = typedArray.getBoolean(R.styleable.BounceLoader_bounce_showShadow, true)
+        this.animDuration = typedArray.getInt(R.styleable.BounceLoader_bounce_animDuration, 1500)
+
+        typedArray.recycle()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-        if (calWidthHeight == 0) {
-            calWidthHeight = 8 * ballRadius
+        if (calWidth == 0 || calHeight == 0) {
+            calWidth = 5 * ballRadius
+            calHeight = 8 * ballRadius
         }
 
-        setMeasuredDimension(calWidthHeight, calWidthHeight)
+        setMeasuredDimension(calWidth, calHeight)
     }
 
     private fun initView() {
         removeAllViews()
         removeAllViewsInLayout()
 
-        if (calWidthHeight == 0) {
-            calWidthHeight = 8 * ballRadius
+        if (calWidth == 0 || calHeight == 0) {
+            calWidth = 5 * ballRadius
+            calHeight = 8 * ballRadius
         }
 
         relativeLayout = RelativeLayout(context)
 
+        if (showShadow) {
+            ballShadowView = CircleView(context = context,
+                    circleRadius = ballRadius,
+                    circleColor = shadowColor,
+                    isAntiAlias = false)
 
-        ballShadowView = CircleView(context = context,
-                circleRadius = ballRadius,
-                circleColor = shadowColor,
-                isAntiAlias = false)
-
-        val shadowParam = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT)
-        shadowParam.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE)
-        shadowParam.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
-        relativeLayout?.addView(ballShadowView, shadowParam)
+            val shadowParam = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+            shadowParam.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE)
+            shadowParam.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+            relativeLayout?.addView(ballShadowView, shadowParam)
+        }
 
         ballCircleView = CircleView(context = context,
                 circleRadius = ballRadius,
@@ -99,7 +116,7 @@ class BounceLoader : LinearLayout, LoaderContract {
         ballParam.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
         relativeLayout?.addView(ballCircleView, ballParam)
 
-        val relParam = RelativeLayout.LayoutParams(calWidthHeight, calWidthHeight)
+        val relParam = RelativeLayout.LayoutParams(calWidth, calHeight)
         this.addView(relativeLayout, relParam)
 
         viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
@@ -129,13 +146,15 @@ class BounceLoader : LinearLayout, LoaderContract {
 
         })
 
-        if (state == STATE_SQUEEZING || state == STATE_RESIZING) {
-            ballShadowView?.clearAnimation()
-            ballShadowView?.visibility = View.GONE
-        } else {
-            ballShadowView?.visibility = View.VISIBLE
-            val shadowAnim = getShadowAnimation()
-            ballShadowView?.startAnimation(shadowAnim)
+        if (showShadow) {
+            if (state == STATE_SQUEEZING || state == STATE_RESIZING) {
+                ballShadowView?.clearAnimation()
+                ballShadowView?.visibility = View.GONE
+            } else {
+                ballShadowView?.visibility = View.VISIBLE
+                val shadowAnim = getShadowAnimation()
+                ballShadowView?.startAnimation(shadowAnim)
+            }
         }
 
         ballCircleView?.startAnimation(ballAnim)
@@ -153,7 +172,7 @@ class BounceLoader : LinearLayout, LoaderContract {
             }
 
             STATE_SQUEEZING -> {
-                ScaleAnimation(1.0f, 1.0f, 1.0f, 0.9f
+                ScaleAnimation(1.0f, 1.0f, 1.0f, 0.85f
                         , ballRadius.toFloat(), (2 * ballRadius).toFloat())
                         .apply {
                             duration = (animDuration / 10).toLong()
@@ -162,7 +181,7 @@ class BounceLoader : LinearLayout, LoaderContract {
             }
 
             STATE_RESIZING -> {
-                ScaleAnimation(1.0f, 1.0f, 0.9f, 1.0f
+                ScaleAnimation(1.0f, 1.0f, 0.85f, 1.0f
                         , ballRadius.toFloat(), (2 * ballRadius).toFloat())
                         .apply {
                             duration = (animDuration / 10).toLong()
@@ -194,8 +213,8 @@ class BounceLoader : LinearLayout, LoaderContract {
 
         when (state) {
             STATE_COMINGUP -> {
-                transAnim = TranslateAnimation(0.0f, (-6 * ballRadius).toFloat(),
-                        0.0f, (-4 * ballRadius).toFloat())
+                transAnim = TranslateAnimation(0.0f, (-4 * ballRadius).toFloat(),
+                        0.0f, (-3 * ballRadius).toFloat())
 
                 scaleAnim = ScaleAnimation(0.9f, 0.5f, 0.9f, 0.5f,
                         ballRadius.toFloat(), ballRadius.toFloat())
@@ -205,8 +224,8 @@ class BounceLoader : LinearLayout, LoaderContract {
                 set.interpolator = DecelerateInterpolator()
             }
             else -> {
-                transAnim = TranslateAnimation((-6 * ballRadius).toFloat(), 0.0f,
-                        (-4 * ballRadius).toFloat(), 0.0f)
+                transAnim = TranslateAnimation((-4 * ballRadius).toFloat(), 0.0f,
+                        (-3 * ballRadius).toFloat(), 0.0f)
 
                 scaleAnim = ScaleAnimation(0.5f, 0.9f, 0.5f, 0.9f,
                         ballRadius.toFloat(), ballRadius.toFloat())
